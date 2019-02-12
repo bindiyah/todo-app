a558b138af3e8dd4ed3c27147a5627b572774a92
bindiyah <bindiya@quintype.com>
2019-02-12 14:56:42 +0530

    basic todo

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-script.js
+function addTask() {
+    var message = prompt("Please enter the tasks to be done");
+    if (message != null) {
+        document.getElementById("completed").innerHTML = message;
+      }
+}
