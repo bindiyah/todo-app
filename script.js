6e74017d3f5ac3e9f5fa7d143707fa7921eb72e9
bindiyah <bindiya@quintype.com>
2019-02-12 15:17:27 +0530

    functionality to add taks

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function addTask() {
     var message = prompt("Please enter the tasks to be done");
     if (message != null) {
-        document.getElementById("completed").innerHTML = message;
+        var res = document.getElementById("tasks");
+        resChild = document.createElement('div');
+        resChild.innerHTML = `<div class="task">
+        <p>${message}</p>
+        <label class="checkbox-main">
+            <input type="checkbox">
+            <span class="checkmark"></span>
+        </label>
+    </div>`;
+        res.appendChild(resChild);
+
       }
 }
